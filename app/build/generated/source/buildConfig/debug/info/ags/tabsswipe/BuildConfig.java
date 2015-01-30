6012f59b61633d8307b99229dc7b810bf8129e1b
vsvankhede <vijay.s.vankhede@gmail.com>
2015-01-30 09:50:20 +0530

    Custom List View is Done... SQlite Operation is done.... Image from gallery we can pick...
    Data from sqlite to listview is done....
    
    Remains
    Display Image from Sqlite.
    Refresh list view when we update data.

--- app/build/generated/source/buildConfig/debug/info/ags/tabsswipe/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/info/ags/tabsswipe/BuildConfig.java
@@ -5,7 +5,7 @@ package info.ags.tabsswipe;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "info.androidhive.tabsswipe";
+  public static final String APPLICATION_ID = "info.ags.tabsswipe";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
